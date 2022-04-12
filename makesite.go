b2f63fbbbb795dfb8a6f8ebe533e40e1fda63c4a
tristann3 <tristan15thompson@gmail.com>
2022-04-11 21:24:48 -0400

    read and rendered text file to a template

--- makesite.go
+++ makesite.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"html/template"
+	"os"
+)
+
+type Post struct {
+	Content string
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	dat, err := os.ReadFile("./first-post.txt")
+    check(err)
+
+	post := Post{Content: string(dat)}
+	parsedTemplate, _ := template.ParseFiles("template.tmpl")
+	newFile, _ := os.Create("first-post.html")
+	err = parsedTemplate.Execute(newFile, post)
+	check(err)
 }
